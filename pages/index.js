cbef66b3fc3f508c84740677c9877045f538320b
cdra0624 <t.adaniya@adawarp.com>
2018-06-01 08:23:02 +0900

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+import React from 'react';
+
+export class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHelloWorldShown: false,
+    };
+    this.toggleHelloWorld = () => {
+      const { isHelloWorldShown } = this.state;
+      this.setState({
+        isHelloWorldShown: !isHelloWorldShown
+      })
+    }
+  }
+
+  render(){
+    const { isHelloWorldShown } = this.state;
+    return (
+      <div>
+        <button
+          onClick={this.toggleHelloWorld}
+        >Toogle hello world</button>
+        {isHelloWorldShown ?
+          <h1>Hello World!</h1> : null}
+      </div>
+    );
+  }
+}
+
+export default Index;
