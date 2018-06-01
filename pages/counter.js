41927b4677fbbcfa4d9b9fab46efbb6ef154dd0e
cdra0624 <t.adaniya@adawarp.com>
2018-06-01 09:26:47 +0900

    chore: add page for counter

---
+++ pages/counter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export class Counter extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render(){
+    const { count } = this.state;
+    return (
+      <div>
+        <button>Count up</button>
+        <h1>{count}</h1>
+      </div>
+    );
+  }
+}
+
+export default Counter;
