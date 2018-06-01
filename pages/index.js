17349f297063b5d7145e7e2703badafc1c73071e
Koki Sonoda <koki.sonoda1304@gmail.com>
2018-06-01 09:29:43 +0900

    add Hello Sonoda

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,47 @@ export class Index extends React.Component {
     super(props);
     this.state = {
       isHelloWorldShown: false,
+      isSonodaShown: false,
     };
     this.toggleHelloWorld = () => {
+      console.log("Hello world");
+      console.log(typeof(this.state.isHelloWorldShown));
       const { isHelloWorldShown } = this.state;
       this.setState({
         isHelloWorldShown: !isHelloWorldShown
       })
     }
+
+    this.toggleSonoda = () => {
+      console.log("Sonoda");
+      const { isSonodaShown } = this.state;
+      this.setState({
+        isSonodaShown: !isSonodaShown
+      })
+    }
   }
 
   render(){
+    console.log("typeof here");
+    console.log(typeof(this.toggleHelloWorld));
     const { isHelloWorldShown } = this.state;
+    const { isSonodaShown } = this.state;
     return (
       <div>
-        <button
-          onClick={this.toggleHelloWorld}
-        >Toogle hello world</button>
-        {isHelloWorldShown ?
-          <h1>Hello World!</h1> : null}
+          <div>
+            <button
+              onClick={this.toggleHelloWorld}
+            >Toogle hello world</button>
+            {isHelloWorldShown ?
+              <h1>Hello World!</h1> : null}
+          </div>
+          <div>
+            <button
+              onClick={this.toggleSonoda}
+            >Toggle sonoda</button>
+            {isSonodaShown ?
+              <h1>Hello Sonoda!</h1> : null}
+          </div>
       </div>
     );
   }
