d90305122a2fbc6f06c1951a646ffeddb4e5602f
Rezaei Rahmat <2249301106@qq.com>
2018-06-01 09:29:02 +0900

    toggle rahmat

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,42 @@ export class Index extends React.Component {
     super(props);
     this.state = {
       isHelloWorldShown: false,
+      isRahmatShow: false,
     };
+    
+
     this.toggleHelloWorld = () => {
       const { isHelloWorldShown } = this.state;
+      console.log(typeof(this.state.isRahmatShow));
       this.setState({
         isHelloWorldShown: !isHelloWorldShown
+      })
+    }
+    this.toggleRahmat = () => {
+      const { isRahmatShow } = this.state; 
+      this.setState({
+        isRahmatShow: !isRahmatShow,
       })
     }
   }
 
   render(){
-    const { isHelloWorldShown } = this.state;
+    const { isHelloWorldShown, isRahmatShow } = this.state;
+    console.log("____type of fucntion_____")
+    console.log(typeof(this.toggleHelloWorld));
     return (
       <div>
-        <button
-          onClick={this.toggleHelloWorld}
-        >Toogle hello world</button>
-        {isHelloWorldShown ?
-          <h1>Hello World!</h1> : null}
+        <div>
+          <button
+            onClick={this.toggleHelloWorld}
+          >Toogle hello world</button>
+          {isHelloWorldShown ?
+            <h1>Hello World!</h1> : null}
+        </div>
+        <div>
+          <button onClick={this.toggleRahmat}>Rahmat</button>
+          {isRahmatShow ? <h1>Hello Rahmat</h1> : null}
+        </div>
       </div>
     );
   }
